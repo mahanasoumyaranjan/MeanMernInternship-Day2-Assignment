808b653d4bc86fcbb40b81d5972afebc7336badf
mahanasoumyaranjan <116441447+mahanasoumyaranjan@users.noreply.github.com>
2023-06-01 11:36:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function printContent(){
+    var pe = document.getElementById("paragraph");
+    pe.innerHTML="What a great IPL this has been, plenty of youngsters added their names to the Indian talent pool, while this was also the tournament of comebacks, so many memorable ones. In the end, it was probably meant to be for MS Dhoni and CSK. What a culmination to a closely contested, thrilling, action-packed IPL. It was great fun bringing all the action to you guys. And now time for goodbyes from IPL 2023, but 'definitely not' from cricket coverage.";
+}
+function hideContent(){
+    var hc = document.getElementById("paragraph");
+    hc.innerHTML="";
+}
